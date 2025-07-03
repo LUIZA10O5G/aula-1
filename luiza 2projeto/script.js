d3a7c191271ed9bee83fd41fb86415dd0e45b928
LUIZA10O5G <diniz.luiza@escola.pr.gov.br>
2025-07-03 11:11:36 -0300

    Add files via upload

---
+++ luiza 2projeto/script.js
@@ -0,0 +1,28 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave  = processaTexto(texto);
+    
+    campoResultado.textContent = palavrasChave.join(', ');
+}
+
+function processa Texto (texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    
+    let frequencias = [];
+    for(let i in palavras) {
+        frequencias [i] = 0;
+        for (let jin palavras) {
+            if (palavras[i]==palavras[j]){
+                frequencias[i]++;
+            }
+        }
+    }
+    console.log(frequencias);
+    
+    return palavras;
+}
